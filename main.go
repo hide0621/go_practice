fb386b7aac9f24715c97e2950e59de41a6a321eb
hide0621 <hi19de72h2so4@gmail.com>
2022-04-04 11:24:25 +0900

    マイクロソフトチュートリアルでのログ管理の学習

--- main.go
+++ main.go
@@ -1,34 +1,18 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"log"
+	"os"
 )
 
-type Employee struct {
-	ID        int
-	FirstName string
-	LastName  string
-	Address   string
-}
-
-//エラーメッセージの作成
-var ErrNotFound = errors.New("Employee not found!")
-
-func getInformation(id int) (*Employee, error) {
-	if id != 1001 {
-		return nil, ErrNotFound
-	}
-
-	employee := Employee{LastName: "Doe", FirstName: "John"}
-	return &employee, nil
-}
-
 func main() {
-	employee, err := getInformation(1000)
-	if errors.Is(err, ErrNotFound) {
-		fmt.Printf("NOT FOUND: %v\n", err)
-	} else {
-		fmt.Print(employee)
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Print("Hey, I'm a log!")
 }
