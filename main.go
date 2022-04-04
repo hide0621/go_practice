b282744b482169d1a8444f962178bb83cc4c22c4
hide0621 <hi19de72h2so4@gmail.com>
2022-04-04 16:25:54 +0900

    udemy講座（ログパッケージについて）

--- main.go
+++ main.go
@@ -6,13 +6,36 @@ import (
 )
 
 func main() {
-	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	//log.SetOutput(os.Stdout)
+
+	/*
+	   log.Print("Log\n")
+	   log.Println("Log2")
+	   log.Printf("Log%d\n", 3)
+	*/
+
+	/*
+	   log.Fatal("Log\n")
+	   log.Fatalln("Log2")
+	   log.Fatalf("Log%d\n", 3)
+	*/
+
+	/*
+	   log.Panic("Log\n")
+	   log.Panicln("Log2")
+	   log.Panicf("Log%d\n", 3)
+	*/
+
+	f, err := os.OpenFile("test.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		//エラーがあればプログラムを終了する
+		return
 	}
 
-	defer file.Close()
+	//test.logを出力先に変更
+	log.SetOutput(f)
 
-	log.SetOutput(file)
-	log.Print("Hey, I'm a log!")
+	//test.logにPrintln関数の実引数を書き込む
+	log.Println("ファイルに書き込む")
+
 }
