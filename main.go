734da6616ba57e45b203eff2551392a701c16b31
hide0621 <hi19de72h2so4@gmail.com>
2022-04-05 11:31:11 +0900

    マイクロソフトのチュートリアルの「メソッドのカプセル化」まで学習

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
 package main
 
-import "fmt"
-
-type triangle struct {
-	size int
-}
-
-type coloredTriangle struct {
-	triangle
-	color string
-}
-
-func (t triangle) perimeter() int {
-	return t.size * 3
-}
-
-func (t coloredTriangle) perimeter() int {
-	return t.size * 3 * 2
-}
+import (
+	"fmt"
+	"gosample/geometry"
+)
 
 func main() {
-	t := coloredTriangle{triangle{3}, "blue"}
-	fmt.Println("Size:", t.size)
-	fmt.Println("Perimeter (colored)", t.perimeter())
-	fmt.Println("Perimeter (normal)", t.triangle.perimeter())
+	t := geometry.Triangle{}
+	t.SetSize(3)
+	//fmt.Println("Size", t.size)
+	fmt.Println("Perimeter", t.Perimeter())
 }
