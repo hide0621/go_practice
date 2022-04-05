da75b764fa86d0bca75f2ea6aa0756b388c1378c
hide0621 <hi19de72h2so4@gmail.com>
2022-04-05 10:39:10 +0900

    マイクロソフトのチュートリアルの「メソッドを埋め込む」まで学習

--- main.go
+++ main.go
@@ -1,41 +1,22 @@
 package main
 
-import (
-	"log"
-	"os"
-)
+import "fmt"
+
+type triangle struct {
+	size int
+}
+
+type coloredTriangle struct {
+	triangle
+	color string
+}
+
+func (t triangle) perimeter() int {
+	return t.size * 3
+}
 
 func main() {
-	//log.SetOutput(os.Stdout)
-
-	/*
-	   log.Print("Log\n")
-	   log.Println("Log2")
-	   log.Printf("Log%d\n", 3)
-	*/
-
-	/*
-	   log.Fatal("Log\n")
-	   log.Fatalln("Log2")
-	   log.Fatalf("Log%d\n", 3)
-	*/
-
-	/*
-	   log.Panic("Log\n")
-	   log.Panicln("Log2")
-	   log.Panicf("Log%d\n", 3)
-	*/
-
-	f, err := os.OpenFile("test.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		//エラーがあればプログラムを終了する
-		return
-	}
-
-	//test.logを出力先に変更
-	log.SetOutput(f)
-
-	//test.logにPrintln関数の実引数を書き込む
-	log.Println("ファイルに書き込む")
-
+	t := coloredTriangle{triangle{3}, "blue"}
+	fmt.Println("Size:", t.size)
+	fmt.Println("Perimeter", t.perimeter())
 }
