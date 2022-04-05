cd9d93edbbb0f3e0d81d05b94eb75f55515e5052
hide0621 <hi19de72h2so4@gmail.com>
2022-04-05 11:01:09 +0900

    マイクロソフトのチュートリアルの「メソッドのオーバーロード」まで学習　

--- main.go
+++ main.go
@@ -15,8 +15,13 @@ func (t triangle) perimeter() int {
 	return t.size * 3
 }
 
+func (t coloredTriangle) perimeter() int {
+	return t.size * 3 * 2
+}
+
 func main() {
 	t := coloredTriangle{triangle{3}, "blue"}
 	fmt.Println("Size:", t.size)
-	fmt.Println("Perimeter", t.perimeter())
+	fmt.Println("Perimeter (colored)", t.perimeter())
+	fmt.Println("Perimeter (normal)", t.triangle.perimeter())
 }
