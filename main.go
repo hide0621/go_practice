b791af7bb9e76d7da064c84dd5d4a3b9e06cfb33
hide0621 <hi19de72h2so4@gmail.com>
2022-04-05 11:45:46 +0900

    マイクロソフトのチュートリアルの「インターフェースの実装」まで学習

--- main.go
+++ main.go
@@ -2,12 +2,49 @@ package main
 
 import (
 	"fmt"
-	"gosample/geometry"
+	"math"
 )
 
+type Shape interface {
+	Perimeter() float64
+	Area() float64
+}
+
+type Square struct {
+	size float64
+}
+
+func (s Square) Area() float64 {
+	return s.size * s.size
+}
+
+func (s Square) Perimeter() float64 {
+	return s.size * 4
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printInformation(s Shape) {
+	fmt.Printf("%T\n", s)
+	fmt.Println("Area: ", s.Area())
+	fmt.Println("Perimeter:", s.Perimeter())
+	fmt.Println()
+}
+
 func main() {
-	t := geometry.Triangle{}
-	t.SetSize(3)
-	//fmt.Println("Size", t.size)
-	fmt.Println("Perimeter", t.Perimeter())
+	var s Shape = Square{3}
+	printInformation(s)
+
+	c := Circle{6}
+	printInformation(c)
 }
