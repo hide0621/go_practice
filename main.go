495e4a0a3d1b2b332aeb93d6b191818ce4bb8bc9
Fujihara_Hideyuki <hi19de72h2so4@gmail.com>
2022-04-07 17:39:23 +0900

    正規のブランチに修正（マイクロソフトのチュートリアル「カスタムサーバーAPIの作成」のコードの不明点を調べてまとめたもの）

--- main.go
+++ main.go
@@ -1,30 +1,44 @@
-//参考　https://qiita.com/yNavS4Ki/items/5b7a0c7c41eb8da8f12a
+//学習サイト　https://docs.microsoft.com/ja-jp/learn/modules/go-methods-interfaces/2-interfaces
+//参考サイト　https://qiita.com/yNavS4Ki/items/5b7a0c7c41eb8da8f12a
+//Sprintfについて　https://qiita.com/Sekky0905/items/5a65602dce83551184b3
+//Fprint系列やSprint系列、Print系列について　https://leben.mobi/go/fmt-print-and-format/go-programming/
+//指定子はこのサイトがわかりやすい　https://leben.mobi/go/fmt-print-and-format/go-programming/
 
 package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-//ハンドラ関数catの作成
-func cat(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "吾輩は猫である")
+//独自型の定義
+type dollars float32
+
+//独自型に紐づけられたStringメソッドを実装
+func (d dollars) String() string {
+	//fmt.Sprintf は、書式指定子に沿ってフォーマットした文字列を標準出力でななく戻り値で返してくれる
+	//dollarsという独自型であり且つ小数点が第2位までの文字列という形で戻り値を返す
+	return fmt.Sprintf("$%.2f", d)
 }
 
-//ハンドラ関数dogの作成
-func dog(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "吾輩は犬である")
+//構造体のマップ型で定義
+//ハンドラ関数でなくハンドラーメソッドでコードを書くので独自型で定義
+type database map[string]dollars
+
+//http.Handler で使用できる ServeHTTP メソッドの実装
+//database型のハンドラー
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		//fmt.Fprint系列は任意の出力先へ表示してくれる
+		//string型に変換したdbオブジェクトを引数wに格納、出力先w（localhost:8000）にそれを表示
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
 }
 
 func main() {
-
-	//マルチプレクサの作成
-	mux := http.NewServeMux()
-
-	// 第二引数にハンドラ関数を渡す
-	mux.HandleFunc("/cat", cat)
-	mux.HandleFunc("/dog", dog)
-	//webサーバーの立ち上げ
-	http.ListenAndServe(":8080", mux)
+	//databaseのオブジェクトを生成
+	db := database{"Go T-Shirt": 25, "Go Jacket": 55}
+	//第一引数のサーバーに対して第二引数のデータを出力するようにハンドラーに指示
+	log.Fatal(http.ListenAndServe("localhost:8000", db))
 }
