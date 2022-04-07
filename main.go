ef395a9ec63c08707587db61f55898499a2870c9
hide0621 <hi19de72h2so4@gmail.com>
2022-04-07 11:41:51 +0900

    マイクロソフトのチュートリアル「カスタムサーバーAPIの作成」まで

--- main.go
+++ main.go
@@ -1,46 +1,32 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
-	"os"
 )
 
-//フィールド(FullName)がスライスになる
-type GitHubResponse []struct {
-	FullName string `json:"full_name"`
+//独自型の定義
+type dollars float32
+
+//独自型に紐づけられたStringメソッドを実装
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
 }
 
-type customWriter struct{}
+//構造体のマップ型で定義
+type database map[string]dollars
 
-func (w customWriter) Write(p []byte) (n int, err error) {
-	//byteのスライスで出力
-	//fmt.Println(p)
-	var resp GitHubResponse
-	//JSONを構造体のデータ(string型)に変換して第二引数に格納、第二引数の構造体は構造体のアドレス
-	json.Unmarshal(p, &resp)
-	//構造体のスライスの形で出力
-	fmt.Println(resp)
-	for _, r := range resp {
-		fmt.Println(r.FullName)
+//http.Handler で使用できる ServeHTTP メソッドの実装
+func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
-	//構造体のスライスの要素数を出力
-	fmt.Println(len(resp))
-	//fmt.Println(len(p))
-	return len(resp), nil //なんでもいいからreturnを返している?,ターミナルに出力されるのは26行目からのfor文の内容と30行目のみ
 }
 
 func main() {
-	resp, err := http.Get("https://api.github.com/users/microsoft/repos?page=15&per_page=5")
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-
-	writer := customWriter{}
-
-	//respに格納されたwebページのbody部分を読み込んでコピー（byte型のスライス）、writer経由でWriteメソッドを呼び出してコピーを引数pに渡してWriteメソッドの結果が返る
-	io.Copy(writer, resp.Body)
+	//databaseのオブジェクトを生成
+	db := database{"Go T-Shirt": 25, "Go Jacket": 55}
+	//第一引数のサーバーに対して第二引数のデータを出力する
+	log.Fatal(http.ListenAndServe("localhost:8000", db))
 }
