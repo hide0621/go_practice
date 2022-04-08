5e634bf4db7ed8e89ad6275d91b8c63e01843453
Fujihara_Hideyuki <hi19de72h2so4@gmail.com>
2022-04-08 14:25:48 +0900

    修正：マイクロソフトのチュートリアル「ゴルーチンとチャネル、バッファありのチャネルについて」

--- main.go
+++ main.go
@@ -27,35 +27,37 @@ func send(ch chan string, message string) {
 
 func main() {
 
-	size := 4
-	ch := make(chan string, size)
-	send(ch, "one")
-	send(ch, "two")
-	send(ch, "three")
-	send(ch, "four")
-	//fmt.Println(ch) //チャネルのアドレス
-	fmt.Println("All data sent to the channel ...")
-
-	for i := 0; i < size; i++ {
-		fmt.Println(<-ch)
-	}
-
-	fmt.Println("Done!")
-
 	/*
-		size := 2
+		size := 4
 		ch := make(chan string, size)
 		send(ch, "one")
 		send(ch, "two")
 		send(ch, "three")
 		send(ch, "four")
-		send(ch, "All data sent to the channel ...")
+		//fmt.Println(ch) //チャネルのアドレス
+		fmt.Println("All data sent to the channel ...")
 
-		for i := 0; i < 4; i++ {
+		for i := 0; i < size; i++ {
 			fmt.Println(<-ch)
 		}
 
 		fmt.Println("Done!")
 	*/
+
+	//ゴルーチンにした対象の処理はバッファのサイズ外
+	size := 2
+	ch := make(chan string, size)
+	send(ch, "one")
+	send(ch, "two")
+	//send(ch, "deadlock") //デッドロック発生
+	go send(ch, "three")
+	go send(ch, "four")
+	fmt.Println("All data sent to the channel ...")
+
+	for i := 0; i < 4; i++ {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Println("Done!")
 
 }
