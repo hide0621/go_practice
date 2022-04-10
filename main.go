04f0ffc78163c2ada3f0af54df8df4f2334d5950
Fujihara_Hideyuki <hi19de72h2so4@gmail.com>
2022-04-11 08:55:53 +0900

    チャネルにclose処理を追加したもの

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 // goroutine
 
 //goroutineは、軽量のスレッドである。
-//goステートメントで関数を指定することで、並行実行される。
+//goステートメントで関数を指定することで、並行処理される。
 //参考：https://qiita.com/taigamikami/items/fc798cdd6a4eaf9a7d5e
 
 //チャネル
 
-//チャネルは、並行実行されるgoroutine間を接続するパイプ(トンネル)のイメージ。
+//チャネルは、並行処理されるgoroutine間を接続するパイプ(トンネル)のイメージ。
 //つまり、並行実行している関数から値を受信する。(あるgoroutineから別のgoroutineへ値を渡す。)
 //参考：https://qiita.com/taigamikami/items/fc798cdd6a4eaf9a7d5e
 
 //チャネルのselectについて
-//selectを利用することで、複数のチャネル操作を待つことができる。
+//selectを利用することで処理対象のイベントを受信するまで、プログラムの実行はブロックさせる
 //受信したものから、画面に表示される。
 
 package main
@@ -29,19 +29,23 @@ func main() {
 	go func() {
 		time.Sleep(4 * time.Second)
 		ch4 <- "four"
+		close(ch4)
 	}()
 
 	go func() {
 		time.Sleep(1 * time.Second)
 		ch1 <- "one"
+		close(ch1)
 	}()
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 10; i++ {
 		select {
 		case msg1 := <-ch1:
 			fmt.Println("received", msg1)
+
 		case msg4 := <-ch4:
 			fmt.Println("received", msg4)
+
 		}
 	}
 
